a5b86cd5356a5277d8569503f6ed485b3cad3a93
Adrian-Alexandru Flisc <fliscadrian@yahoo.com>
2023-04-24 11:28:22 +0000

    ORA_SOLUTION_24_04_2023

--- python/guess-number/NumberGuessingService.py
+++ python/guess-number/NumberGuessingService.py
@@ -28,12 +28,44 @@ The 2 is in my number somewhere.
 But in the wrong place.
 
 """
+def numer_digits(number):
+     return_arr = []
+     while number // 10 > 0:
+          return_arr.append(number % 10)
+          number = number // 10
+     return_arr.append(number)
+     return return_arr
 
 def main():
-  print("Hello, My Dear Friend! I randomly generated a number between 0 and 100000. Can you find out?\n")
+    print("Hello, My Dear Friend! I randomly generated a number between 0 and 100000. Can you find out?\n")
 
-  newNumber = random.randint(0,100000)
-#   guess = int(input())
+    newNumber = random.randint(0,200)
+    newNumberDigits = numer_digits(newNumber)
+#     print(newNumber)
+#     print(newNumberDigits)
+#     return
+     
+    guess = int(input())
+    while guess != newNumber:
+        
+        if guess < newNumber:          
+            print("Your guess is smaller than my number.")
+        else:
+            print("Your guess is higher than my number.")
+
+        guess_digits = numer_digits(guess)
+     #    print(guess_digits)
+        for i in range(len(guess_digits) - 1, -1, -1):
+             if guess_digits[i] in newNumberDigits:
+                  print("The ", guess_digits[i], " is in my number somewhere.")
+                  if i < len(newNumberDigits) and guess_digits[i] == newNumberDigits[i]:
+                       print("And in the right place.")
+                  else:
+                       print("But in the wrong place.")
+        guess = int(input())
+    print("You won! Your guess is equal to my number.")
+
+
 #   print(guess)
 
 if __name__ == "__main__":
